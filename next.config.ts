7936d5826de6600a2fd7c1039a23c3fb6cc2ea8e
Tarun Singh <atypicalesper@gmail.com>
2026-03-25 11:00:00 +0530

    add GitHub Actions workflow, static export for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
   experimental: {
     turbopackFileSystemCacheForDev: true,
-  }
+  },
 };
 
 export default nextConfig;
